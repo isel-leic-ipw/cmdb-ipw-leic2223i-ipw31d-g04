892e9700678c0061389498c06f707f521d306ec8
goncaloa49417 <113713317+goncaloa49417@users.noreply.github.com>
2022-11-18 23:30:49 +0000

    Update cmdb-services.mjs

--- Parte1/cmdb-services.mjs
+++ Parte1/cmdb-services.mjs
@@ -1,16 +1,19 @@
 
+// não pode ter saber nem ter conhecimento do http,só tem a ver com a logica da aplicaçãoe, nao pode saber nada do request,response
+//este modulo tem como opera~çoes/meotod neste caso exportar como o gettask
+//todos recebem como parametro o taskID(que é o que vem no equest.params.id), mas supostamente eles nao podiam ter acesso a tudo que envolve http
 import data from './cmdb-data-mem.mjs'
 
-function getGroups(){
-    return data.getGroups()     //validar se existe grupos. N é necessário pois se n há grupos, n imprime nada
+async function  getGroups(){//obtem os grupos todos, não precisa de parametros
+    return  data.getGroups()     //validar se existe grupos. N é necessário pois se n há grupos, n imprime nada
 }
 
-function createGroup(name, desc){
+async function createGroup(name, desc){
     //é necessário validar o nome
     return data.createGroup(name, desc)
 }
 
-function getGroupById(id){
+async function getGroupById(id){
 
 }
 
