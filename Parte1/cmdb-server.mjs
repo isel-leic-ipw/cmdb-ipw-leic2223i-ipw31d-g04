b24570fa4e7d286f15c1947e83bf435087bab19a
48268 - Marcorio Fortes <a48268@alunos.isel.pt>
2022-11-20 08:41:55 +0000

    1

--- Parte1/cmdb-server.mjs
+++ Parte1/cmdb-server.mjs
@@ -10,13 +10,13 @@ const PORT = 1904
 
 app.use(express.json())   //se o body tiver em formato json na transforma o body json num objeto request
 
-app.get("/group",webApi.getGroups)  // obter todos os grupos
-app.get("/group/:groupId",webApi.getGroup) // obter um grupo
-app.delete("/group/:groupId",webApi.deleteGroup) // obter um grupo
-app.post("/group",webApi.createGroup) // criar grupo
-app.post("/group/:groupId/movies",webApi.addMovieToGroup) // obter um grupo
-app.delete("/group/:groupId/movies/:movieId",webApi.removeMovieFromGroup) // obter um grupo
-app.put("/group/:groupId",webApi.updateGroup) // editar grupo
+app.get("/groups",webApi.getGroups)  // obter todos os grupos
+app.get("/groups/:groupId",webApi.getGroup) // obter um grupo
+app.delete("/groups/:groupId",webApi.deleteGroup) // obter um grupo
+app.post("/groups",webApi.createGroup) // criar grupo
+app.post("/groups/:groupId/movies",webApi.addMovieToGroup) // obter um grupo
+app.delete("/groups/:groupId/movies/:movieId",webApi.removeMovieFromGroup) // obter um grupo
+app.put("/groups/:groupId",webApi.updateGroup) // editar grupo
 
 app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
 //app.listen(Port, ()=>console.log("Listening on PORT:" + Port))    //esta mal temos que alterar
