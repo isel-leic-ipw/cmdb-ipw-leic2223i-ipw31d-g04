7a55b3d0d4d7a1556540dcc55afe194b105d2601
goncaloa49417 <113713317+goncaloa49417@users.noreply.github.com>
2022-11-20 03:08:04 +0000

    Update cmdb-web-api.mjs

--- Parte1/cmdb-web-api.mjs
+++ Parte1/cmdb-web-api.mjs
@@ -1,23 +1,44 @@
-
 //webapi corresponde ao http -api
 //put e get, implementações parecidas
 import services from './cmdb-services.mjs'
 
  export async function getGroups(req,response){
     console.log(req.query)
-   return response.json( services.getGroups())// nao é recursivo apenas vai buscar ao services
+   return response.json( services.getGroups())
 }
 
-export async function createGroup(req,response){// esta funcao é toda marada man
+export async function createGroup(req,rsp){
     console.log(req.body)
-   await services.createGroup(req.body.name,req.body.desc)
-            .then(g=> response.json(g))// ver o que then faz, ja nao me lembro
+   let g= await services.createGroup(req.body.name,req.body.desc)
+         rsp.status(201).json(g)  
 }
 
-export async function getGroupById(req, response){
-
+export async function getGroupById(req, rsp){
+   let a= await services.getGroupById(req.body.name,req.body.desc)
+    rsp.status(200).json(a)
 }
 
+async function deletedGroup(req, rsp) {
+    const id = req.params.id
+    const deleted = await tasksServices.deleteTask(id)
+    if(deleted) {
+        rsp.status(200).json({status: `group with id ${id} deleted with success`})
+    } else {
+        rsp.status(404).json({error: `group with id ${id} not found`})
+    }
+}
+
+async function editGroup(req,rsp){
+    getTaskAndAct(req.params.id, rsp, update)
+
+    function update(group) {
+        rsp.status(201)
+        rsp.json({
+            status: `group with id ${id} updated with success`,
+            newgroup: group
+            })
+    }
+}
 //editGroup --> PUT
 
 //deleteGroup --> DELETE
@@ -28,7 +49,18 @@ export const webapi = {
     getGroups,
     createGroup,
     getGroupById,
+    editGroup,
+    deletedGroup
 }
 
 
 export default webapi//nao entendi o que é isto
+
+async function getTaskAndAct(id, rsp, action) {
+    const group = await services.getTask(id)
+    if(group != undefined) {
+        action(group)
+    } else {
+        rsp.status(404).json({error: `group with id ${id} not found`})
+    }
+}
