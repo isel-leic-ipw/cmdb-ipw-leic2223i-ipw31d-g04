bc5c83ef22ac6019bc6f4328adeb00b9f71faee7
goncaloa49417 <113713317+goncaloa49417@users.noreply.github.com>
2022-11-23 21:23:42 +0000

    Update cmdb-services.mjs

--- Parte1/services/cmdb-services.mjs
+++ Parte1/services/cmdb-services.mjs
@@ -1,27 +1,54 @@
 // não pode ter saber nem ter conhecimento do http,só tem a ver com a logica da aplicaçãoe, nao pode saber nada do request,response
 //este modulo tem como opera~çoes/meotod neste caso exportar como o gettask
 //todos recebem como parametro o taskID(que é o que vem no equest.params.id), mas supostamente eles nao podiam ter acesso a tudo que envolve http
+//new coments
+//o invalidargument foi usado para quando o erro for para a web api ser em objeto,e nao string para o catch nao ter que fazer passing a string 
 import * as groupsData from '../data/cmdb-data-mem.mjs'
+import * as usersData from './users-data.mjs'
 import errors from '../errors.mjs'
 
-export async function  getGroups(){//obtem os grupos todos, não precisa de parametros
-    return  groupsData.getGroups()     //validar se existe grupos. N é necessário pois se n há grupos, n imprime nada
-}
 
-export async function getGroupsById(groupId){
-    return groupsData.getGroupById(groupId)
+export async function  getGroups(userToken){// falta muita coisa neste
+    return  groupsData.getGroups(userToken)   
+
+export async function getGroupsById(groupId,userToken){// alterar para em vez de token ser user ID
+    const user = await usersData.getUser(userToken)
+    if(!user) {
+        throw errors.USER_NOT_FOUND()
+    }
+    const group = await tasksData.getGroupsById(user.id, groupId)
+    if(group) {
+        return group
+    }
+
+    throw errors.TASK_NOT_FOUND(groupId)
 
 }
-export async function createGroup(groupToCreate){
-    //é necessário validar o nome
-    return groupsData.createGroup(groupToCreate)
+export async function createGroup(groupToCreate,userToken){
+    const user = await usersData.getUser(userToken)
+    if(!user) {
+        throw errors.USER_NOT_FOUND()
+    }
+    if(!isAString(userToken, groupToCreate.name)) {
+         throw errors.INVALID_PARAMETER('name')
+    }
+    if(!isAString(userToken, groupToCreate.description)) {
+        throw errors.INVALID_PARAMETER('description')
+   }
+
+
+    return tasksData.createTask(user.id, groupToCreate)
 }
 
 export async function deletedGroup(id){
     return groupsData.deleteGroup(id)
 }
+}
 
-export async function updateGroup(groupId, groupToUpdate){
+export async function updateGroup(groupId, groupToUpdate,userToken){
+    if(NaN(user))
+    throw errors.USER_NOT_FOUND(user.Id)
+    
     if(!isAString(groupToUpdate.name))
     throw errors.INVALID_ARGUMENT(groupToUpdate.name)
     if(!isAString(groupToUpdate.description))
@@ -34,3 +61,9 @@ export async function updateGroup(groupId, groupToUpdate){
 function isAString(value) {
     return typeof value == 'string' && value != ""
 }
+
+function NaN(value) {
+    return typeof value != Number && value != "" 
+
+}
+
