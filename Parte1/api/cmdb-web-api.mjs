c68cf09f20d1ef9836071b9aeb70b7da90e86db0
goncaloa49417 <113713317+goncaloa49417@users.noreply.github.com>
2022-11-24 01:00:25 +0000

    Update cmdb-web-api.mjs

--- Parte1/api/cmdb-web-api.mjs
+++ Parte1/api/cmdb-web-api.mjs
@@ -1,42 +1,43 @@
 //verifyauthentication é usado para verificar se ha token se nao ha dá logo resposta;
 //é uma funcao intermedia
-import * as groupServices from '../services/cmdb-services.mjs'
+import * as groupServices from './cmdb-services.mjs'
+import toHttpResponse from './response-errors.mjs'
 //exporta para o exterior,que tem o valor retornado pela funcao verifyAuthentication
 export const getGroups = verifyAuthentication(getGroupsInternal)
-export const getGroupsById = verifyAuthentication(getGroupsByIdInternal)
+export const getGroupsById = verifyAuthentication(getGroupInternal)
 export const deleteGroup = verifyAuthentication(deleteGroupInternal)
 export const updateGroup = verifyAuthentication(updateGroupInternal)
 export const createGroup= verifyAuthentication(createGroupInternal)
 
- export async function getGroupsInternal(req,rsp){
-    const groups = await groupServices.getGroups()
+ export async function getGroupsInternal(req,rsp){// incompleto
+    const groups = await groupServices.getGroups(req.token)
     rsp.json(groups)
 }
 
-export async function getGroupsByIdInternal(req,rsp){
+export async function getGroupInternal(req,rsp){
   await getGroupAndAct(req.params.groupId, rsp, group => rsp.json(group).status(200))
+  return groupServices.getGroupsById(req.params.groupId,req.token)
 }
 
 export async function createGroupInternal(req,rsp){
     try {
-        let newGroup = await groupServices.createGroup(req.body)
+        let newGroup = await groupServices.createGroup(req.token,req.body)
         rsp
             .status(201)
             .json({
-                status: `Task with id ${newGroup.id} created with success`,
+                status: `Task with id ${newGroup.id} created with success`,// nao sei se é id ou groupId, se o valor newgroup for do request entao é groupId se nao é id
                 newGroup: newGroup
             })
 
     } catch(e) {
-        rsp
-            .status(400)
-            .json({error: `Error creating task: ${e}`})
+        let ret = toHttpResponse(e)
+        rsp.status(ret.status).json(ret.body)
     }
 }
 
 export async function deleteGroupInternal(req, rsp) {
     const groupId = req.params.groupId
-    const deleted = await groupServices.deletedGroup(groupId)
+    const deleted = await groupServices.deletedGroup(req.token,groupId)
     if(deleted) {
         rsp.status(200).json({status: `group with id ${groupId} deleted with success`})
     } else {
@@ -79,11 +80,10 @@ function verifyAuthentication(handlerFunction) {
         }
         req.token = userToken 
         handlerFunction(req, rsp)
-    
+        
     }
     
 }
-
 export function addMovieToGroup (req,rsp){
 
 }
