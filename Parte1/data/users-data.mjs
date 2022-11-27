8c1bf587b41972a7155b2a3798e047e02fa58c47
goncaloa49417 <113713317+goncaloa49417@users.noreply.github.com>
2022-11-27 18:00:45 +0000

    Update users-data.mjs

--- Parte1/data/users-data.mjs
+++ Parte1/data/users-data.mjs
@@ -1,15 +1,16 @@
-const NUM_USERS = 2
+const NUM_USERS = 2    
+let newUser=NUM_USERS
 
 let users = new Array(NUM_USERS).fill(0, 0, NUM_USERS)
-    .map((_, idx) => {
+    .map((_, idx) => { 
         return {
             id: idx,
             name: `User ${idx}`,
             email: `User${idx}@slb.pt`,
             token: "ef604e80-a351-4d13-b78f-c888f3e63b6" + idx
-        }
+        } 
     })
-let maxId = NUM_USERS
+
 
 
 export async function getUser(userToken) {
@@ -18,7 +19,18 @@ export async function getUser(userToken) {
 
 export async function createNewUser(){
 
+let newGroup = {     //x = grupo
+    id:getNewUserId(),
+    name: `User ${id}`,
+    email: `User${id}@slb.pt`,
+    token: "ef604e80-a351-4d13-b78f-c888f3e63b6" + id
+} 
+
+groups.push(newGroup)
+return newGroup  //resolver a promessa de criar o grupo
 }
-function getNewId (){
-    return maxId++
+
+
+function getNewUserId() {
+    return newUser++
 }
